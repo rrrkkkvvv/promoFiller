315f5e19fd42b643c484c82615038ba8a7d20c7c
Roman Karpii <romankarpiy8@gmail.com>
2025-04-27 01:38:03 +0200

    init

---
+++ content.js
@@ -0,0 +1,34 @@
+const socket = new WebSocket("ws://localhost:8080");
+ 
+socket.addEventListener("message", function (event) {
+    const {type, text} = JSON.parse(event.data);
+    if(type==="coupon"){
+      const caseBattleCoupon = document.querySelector(".block__coupon").querySelector("input");  
+      const button = document.querySelector(".block__coupon").querySelector("button");  
+
+      if (caseBattleCoupon  && button) {
+         caseBattleCoupon.value = text;
+        
+        // setTimeout(()=>{
+        //   button.click();
+        // }, 1000)
+        
+      }
+    }else if(type==="promo"){
+    
+      const dinoPromo = document.querySelector("input.NewPromoInput_input__ljgpW");
+  
+      const button = document.querySelector("button.NewPromoInput_acceptBtn__gVjBs");  
+  
+    if (dinoPromo && button) {
+      dinoPromo.value = text;
+ 
+ 
+
+      setTimeout(()=>{
+        button.click();
+      }, 150)
+    }
+  }
+
+});
